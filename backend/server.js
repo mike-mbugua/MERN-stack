bd9bc4e96d450a41c19fc77d70350903be524340
mike-mbugua <mikembugua960@gmail.com>
2023-08-28 22:43:05 +0300

    create a simple way to use the routes

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,7 @@ const dotenv=require("dotenv").config();
 const app=express();
 const port=process.env.PORT || 5000;
 
+app.use('/api/goals',require('./routes/routes.js'))
+
 
 app.listen(port,()=> console.log(`server listening to port ${port}`))
