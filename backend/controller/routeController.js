31bc374bc0ad9158d040fda693a45c285947e09e
mike-mbugua <mikembugua960@gmail.com>
2023-08-30 12:43:53 +0300

    add async handler

--- backend/controller/routeController.js
+++ backend/controller/routeController.js
@@ -1,25 +1,34 @@
+const asyncHandler = require('express-async-handler')
+
 // to get all goals
-const getGoal = (req,res)=>{
+const getGoal = asyncHandler(async(req,res)=>{
     res.status(200).json({msg: "the goals"})
-}
+})
 
 
 // to create new goals
-const setGoal=(req,res)=>{
+const setGoal= asyncHandler(async(req,res)=>{
+    // for you to create a goal you have to use a middleware in the server.js 
+console.log(req.body)
+
+    if(!req.body.text){
+        res.status(400)
+        throw new Error("Please fill the text field");
+    }
     res.status(200).json({msg: "goal created"})
-}
+})
 
 
 // to update a specific goal
-const updateGoal=(req,res)=>{
+const updateGoal= asyncHandler(async(req,res)=>{
     res.status(200).json({msg: `goal with id of ${req.params.id} has been updated`})
-}
+})
 
 
-// to delete a specific goal
-const deleteGoal=(req,res)=>{
+// to delete a specific  goal
+const deleteGoal= asyncHandler(async(req,res)=>{
     res.status(200).json({msg: `goal with id of  ${req.params.id} has been deleted`})
-}
+})
 
 module.exports = {
     getGoal,
