4f6b4c1f3e936c7aa5c334dde2577a7c075f36aa
mike-mbugua <mikembugua960@gmail.com>
2023-08-30 12:42:59 +0300

    using the middle ware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express= require("express");
 const dotenv=require("dotenv").config();
 const app=express();
+const colors=require('colors')
+const connectDB = require('./config/db')
+const {errorHandler} = require('./middleware/errorMiddleare')
 const port=process.env.PORT || 5000;
+
+connectDB()
+
+// here isthe middleware to allow to post goals with body
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 app.use('/api/goals',require('./routes/routes.js'))
 
+app.use(errorHandler)
 
 app.listen(port,()=> console.log(`server listening to port ${port}`))
